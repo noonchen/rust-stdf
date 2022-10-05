a415748e538c6e0eab3c9044cf010adf2b1774ec
Zhengwu <chennoon233@foxmail.com>
2022-10-05 23:18:25 +0800

    add new example code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod stdf_file;
 mod stdf_error;
 mod stdf_types;
-use stdf_types::StdfRecordType;
+use stdf_types::{StdfRecord, stdf_record_type::*};
 use std::env;
 use std::time::Instant;
 
@@ -21,14 +21,35 @@ fn main() {
             return;}
     };
     
+    // let start_time = Instant::now();
+    // if let Ok(rec_list) = reader.read_all_records() {
+    //     let elapsed = start_time.elapsed().as_millis();
+    //     println!("Total records: {}, time elapsed {} ms\n", rec_list.len(), elapsed);
+    //     for rec in rec_list.iter().filter(|x| x.is_type(StdfRecordType::RecMIR)) {
+    //         println!("{:?}", rec);
+    //     }
+    // }
+
     let start_time = Instant::now();
-    if let Ok(rec_list) = reader.read_all_records() {
-        let elapsed = start_time.elapsed().as_millis();
-        println!("Total records: {}, time elapsed {} ms\n", rec_list.len(), elapsed);
-        for rec in rec_list.iter().filter(|x| x.is_type(StdfRecordType::RecMIR)) {
-            println!("{:?}", rec);
-        }
+    let mut count = 0usize;
+    let rec_t = REC_PIR | REC_PRR;
+    for (ind, rec) in reader
+                                            .get_record_iter()
+                                            .filter(|x| x.is_type(rec_t))
+                                            .enumerate() {
+        count = ind + 1;
+        match rec {
+            StdfRecord::PIR(pir_rec) => {
+                println!("x: {}, y: {}", pir_rec.head_num, pir_rec.site_num)
+            },
+            StdfRecord::PRR(prr_rec) => {
+                println!("x: {}, y: {}", prr_rec.x_coord, prr_rec.y_coord)
+            },
+            _ => {},
+        };
     }
+    let elapsed = start_time.elapsed().as_millis();
+    println!("Total {:?} records: {}, time elapsed {} ms\n", rec_t, count, elapsed);
 
 }
 
